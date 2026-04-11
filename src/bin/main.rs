a72a145ebd517f3d75919023aa431e77f2493572
kralonur <kralonur1998@gmail.com>
2026-04-11 04:16:28 +0300

    feat: use mcpwm

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -12,12 +12,8 @@ use embassy_executor::Spawner;
 use embassy_time::{Duration, Timer};
 use esp_hal::{
     clock::CpuClock,
-    gpio::{DriveMode, Level, Output, OutputConfig},
-    ledc::{
-        LSGlobalClkSource, Ledc, LowSpeed,
-        channel::{self, ChannelIFace},
-        timer::{self, TimerIFace},
-    },
+    gpio::{Level, Output, OutputConfig},
+    mcpwm::{McPwm, PeripheralClockConfig},
     time::Rate,
     timer::timg::TimerGroup,
 };
@@ -27,11 +23,16 @@ use {esp_backtrace as _, esp_println as _};
 // For more information see: <https://docs.espressif.com/projects/esp-idf/en/stable/esp32/api-reference/system/app_image_format.html#application-description>
 esp_bootloader_esp_idf::esp_app_desc!();
 
+const PWM_PERIOD_TICKS: u16 = 99;
 const PWM_FREQUENCY_KHZ: u32 = 20;
-const PWM_MIN_DUTY_PERCENT: u8 = 10;
-const PWM_MAX_DUTY_PERCENT: u8 = 100;
+const PWM_MIN_DUTY_PERCENT: u16 = 10;
+const PWM_MAX_DUTY_PERCENT: u16 = 100;
 const RAMP_STEP_MS: u64 = 50;
 const DIRECTION_CHANGE_DELAY_MS: u64 = 500;
+
+fn duty_pct_to_timestamp(duty_pct: u16) -> u16 {
+    (PWM_PERIOD_TICKS * duty_pct) / 100
+}
 
 #[allow(
     clippy::large_stack_frames,
@@ -52,59 +53,53 @@ async fn main(spawner: Spawner) -> ! {
     let timg0 = TimerGroup::new(peripherals.TIMG0);
     esp_rtos::start(timg0.timer0);
 
+    let clock_cfg = PeripheralClockConfig::with_frequency(Rate::from_mhz(40)).unwrap();
+    let mut mcpwm = McPwm::new(peripherals.MCPWM0, clock_cfg);
+    mcpwm.operator0.set_timer(&mcpwm.timer0);
+    mcpwm.operator1.set_timer(&mcpwm.timer0);
+
+    let mut left_pwm = mcpwm.operator0.with_pin_a(
+        left_pwm_pin,
+        esp_hal::mcpwm::operator::PwmPinConfig::UP_ACTIVE_HIGH,
+    );
+
+    let mut right_pwm = mcpwm.operator1.with_pin_a(
+        right_pwm_pin,
+        esp_hal::mcpwm::operator::PwmPinConfig::UP_ACTIVE_HIGH,
+    );
+
+    let timer_clock_cfg = clock_cfg
+        .timer_clock_with_frequency(
+            PWM_PERIOD_TICKS,
+            esp_hal::mcpwm::timer::PwmWorkingMode::Increase,
+            Rate::from_khz(PWM_FREQUENCY_KHZ),
+        )
+        .unwrap();
+    mcpwm.timer0.start(timer_clock_cfg);
+
     let mut left_enable = Output::new(left_enable_pin, Level::High, OutputConfig::default());
     let mut right_enable = Output::new(right_enable_pin, Level::High, OutputConfig::default());
 
-    let mut ledc = Ledc::new(peripherals.LEDC);
-    ledc.set_global_slow_clock(LSGlobalClkSource::APBClk);
-
-    let mut pwm_timer = ledc.timer::<LowSpeed>(timer::Number::Timer0);
-    pwm_timer
-        .configure(timer::config::Config {
-            duty: timer::config::Duty::Duty10Bit,
-            clock_source: timer::LSClockSource::APBClk,
-            frequency: Rate::from_khz(PWM_FREQUENCY_KHZ),
-        })
-        .unwrap();
-
-    let mut left_pwm = ledc.channel(channel::Number::Channel0, left_pwm_pin);
-    left_pwm
-        .configure(channel::config::Config {
-            timer: &pwm_timer,
-            duty_pct: 0,
-            drive_mode: DriveMode::PushPull,
-        })
-        .unwrap();
-
-    let mut right_pwm = ledc.channel(channel::Number::Channel1, right_pwm_pin);
-    right_pwm
-        .configure(channel::config::Config {
-            timer: &pwm_timer,
-            duty_pct: 0,
-            drive_mode: DriveMode::PushPull,
-        })
-        .unwrap();
-
     info!(
-        "motor ramp active, left/right pwm alternate, duty {}-{}%, freq={}kHz",
-        PWM_MIN_DUTY_PERCENT, PWM_MAX_DUTY_PERCENT, PWM_FREQUENCY_KHZ,
+        "motor ramp active, left/right pwm alternate, duty {}-{}%, freq={}kHz, period={}",
+        PWM_MIN_DUTY_PERCENT, PWM_MAX_DUTY_PERCENT, PWM_FREQUENCY_KHZ, PWM_PERIOD_TICKS,
     );
 
     let _ = spawner;
 
     loop {
-        right_pwm.set_duty(0).unwrap();
+        right_pwm.set_timestamp(0);
         for duty in PWM_MIN_DUTY_PERCENT..=PWM_MAX_DUTY_PERCENT {
-            left_pwm.set_duty(duty).unwrap();
+            left_pwm.set_timestamp(duty_pct_to_timestamp(duty));
             Timer::after(Duration::from_millis(RAMP_STEP_MS)).await;
         }
 
         for duty in (PWM_MIN_DUTY_PERCENT..PWM_MAX_DUTY_PERCENT).rev() {
-            left_pwm.set_duty(duty).unwrap();
+            left_pwm.set_timestamp(duty_pct_to_timestamp(duty));
             Timer::after(Duration::from_millis(RAMP_STEP_MS)).await;
         }
 
-        left_pwm.set_duty(0).unwrap();
+        left_pwm.set_timestamp(0);
         left_enable.set_low();
         right_enable.set_low();
         Timer::after(Duration::from_millis(DIRECTION_CHANGE_DELAY_MS)).await;
@@ -112,16 +107,16 @@ async fn main(spawner: Spawner) -> ! {
         right_enable.set_high();
 
         for duty in PWM_MIN_DUTY_PERCENT..=PWM_MAX_DUTY_PERCENT {
-            right_pwm.set_duty(duty).unwrap();
+            right_pwm.set_timestamp(duty_pct_to_timestamp(duty));
             Timer::after(Duration::from_millis(RAMP_STEP_MS)).await;
         }
 
         for duty in (PWM_MIN_DUTY_PERCENT..PWM_MAX_DUTY_PERCENT).rev() {
-            right_pwm.set_duty(duty).unwrap();
+            right_pwm.set_timestamp(duty_pct_to_timestamp(duty));
             Timer::after(Duration::from_millis(RAMP_STEP_MS)).await;
         }
 
-        right_pwm.set_duty(0).unwrap();
+        right_pwm.set_timestamp(0);
         left_enable.set_low();
         right_enable.set_low();
         Timer::after(Duration::from_millis(DIRECTION_CHANGE_DELAY_MS)).await;
